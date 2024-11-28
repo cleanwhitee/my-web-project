7f02cf3268cdac15e2473a54bd96c1dafea9883e
Beyza <beyzatasci122@gmail.com>
2024-11-28 16:08:23 +0300

    index.vue dosyası oluşturuldu ve FontAwesome icon paketi indirildi

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+
+  // Font Awesome CSS ekleme
+  css: [
+    '@fortawesome/fontawesome-free/css/all.css'  // Font Awesome'ı dahil ediyoruz
+  ],
+  
 })
---
+++ pages/index.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="page">
+      <h1>Hoş Geldiniz!</h1>
+      <p>Bu bir örnek sayfadır.</p>
+
+ 
+</div>
+</template>
+<style scoped>
+.page {
+  text-align: center;
+}
+
+.page {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  padding: 10px;
+}
+
+.main-content {
+  text-align: center;
+  padding: 20px;
+}
+
+h1 {
+  font-size: 40px;
+}
+
+.tabs {
+  display: flex;
+  justify-content: center;
+  margin-top: 20px;
+}
+
+.tab {
+  font-size: 18px;
+  padding: 10px 20px;
+  cursor: pointer;
+  margin-right: 30px;
+}
+
+.tab.active {
+  font-weight: bold;
+  color: #d14b88; /* Aktif sekme için renk */
+  border-bottom: 2px solid #d14b88; /* Alt çizgi */
+}
+
+.tab-content {
+  margin-top: 20px;
+}
+
+.tab-content h2 {
+  font-size: 40px;
+}
+
+</style>
